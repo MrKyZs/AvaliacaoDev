05e3dec80e1fed1ba681417284414073d9a2e944
Guilherme <guimarquestavares@gmail.com>
2024-08-09 22:22:58 -0300

    Projeto AvaliacaoDev Formação#AGER com todos os itens da lista e algumas alterações a mais

--- src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
+++ src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
@@ -1,2 +1,26 @@
 -- CREATE TABLE exame (rowid bigint auto_increment, nm_exame VARCHAR(255));
 -- INSERT INTO exame (nm_exame) VALUES ('Acuidade Visual'), ('Urina'), ('Clinico'), ('Sangue');
+
+
+-- MEUS SCRIPTS
+
+-- CREATE TABLE exame (
+--rowid BIGINT AUTO_INCREMENT, 
+--nm_exame VARCHAR(255),
+--PRIMARY KEY(rowid)
+--);
+
+--CREATE TABLE funcionario (
+--rowid BIGINT AUTO_INCREMENT, 
+--nm_funcionario VARCHAR(255),
+--PRIMARY KEY(rowid)
+--);
+
+--CREATE TABLE exame_funcionarios(
+--	rowid INT AUTO_INCREMENT PRIMARY KEY,
+--    dataExame DATE NOT NULL,
+--    cd_funcionario BIGINT NOT NULL,
+--    cd_exame BIGINT NOT NULL,
+--    FOREIGN KEY(cd_funcionario) REFERENCES funcionario(rowid),
+--    FOREIGN KEY(cd_exame) REFERENCES exame(rowid)
+--);
